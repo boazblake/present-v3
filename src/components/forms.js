96087fc0e69689499e9e07092b902cc9e0bc0b56
bblake <bblake@goempyrean.com>
2022-09-13 10:05:35 -0500

    b

--- src/components/forms.js
+++ src/components/forms.js
@@ -22,106 +22,106 @@ const addPresentationTask = ({ mdl, state }) => {
   return mdl.http.postTask(mdl, "presentations", presentation)
 }
 
-const editPresentationTask = ({ mdl, state: { title }, key }) => {
+const editPresentationTask = ({ mdl, state: { title }, key }) =>
   mdl.http.postTask(mdl, `presentations/${key}`, { title })
 
 
-  const save = ({ mdl, state, key }) => {
-    const onError = error => {
-      state.error = error;
-      alert(state.error)
-    }
-
-    const onSuccess = () => {
-      state.title = ""
-      mdl.state.showModal = false
-      loadAllPresentationsTask(mdl)
-    }
-
-    validateStateTask(state).chain(state =>
-      state.add ? addPresentationTask({ mdl, state }) : editPresentationTask({ mdl, state, key })
-    ).fork(onError, onSuccess)
+const save = ({ mdl, state, key }) => {
+  const onError = error => {
+    state.error = error;
+    alert(state.error)
   }
 
-
-
-
-  const addSlide = (mdl, state) => {
-    const slide = SLIDE(state.title, currentPresentationId())
-
-    const onError = log("error")
-    const onSuccess = () => {
-      state.title = ""
-      mdl.state.showModal = false
-      loadSlidesByProjectId(mdl, currentPresentationId())
-    }
-
-    mdl.http.postTask(mdl, "slides", slide).fork(onError, onSuccess)
+  const onSuccess = () => {
+    state.title = ""
+    mdl.state.showModal = false
+    loadAllPresentationsTask(mdl)
   }
 
-  const NewPresentationForm = ({ attrs: { title } }) => {
-    const state = {
-      title: "",
-      add: true
-    }
+  validateStateTask(state).chain(state =>
+    state.add ? addPresentationTask({ mdl, state }) : editPresentationTask({ mdl, state, key })
+  ).fork(onError, onSuccess)
+}
 
-    if (title) {
-      state.title = title
-      state.add = false
-    }
 
-    return {
-      view: ({ attrs: { mdl, key } }) =>
+
+
+const addSlide = (mdl, state) => {
+  const slide = SLIDE(state.title, currentPresentationId())
+
+  const onError = log("error")
+  const onSuccess = () => {
+    state.title = ""
+    mdl.state.showModal = false
+    loadSlidesByProjectId(mdl, currentPresentationId())
+  }
+
+  mdl.http.postTask(mdl, "slides", slide).fork(onError, onSuccess)
+}
+
+const NewPresentationForm = ({ attrs: { title } }) => {
+  const state = {
+    title: "",
+    add: true
+  }
+
+  if (title) {
+    state.title = title
+    state.add = false
+  }
+
+  return {
+    view: ({ attrs: { mdl, key } }) =>
+      m(
+        "form.w3-container.w3-card.w3-white",
+        { onsubmit: (e) => e.preventDefault() },
         m(
-          "form.w3-container.w3-card.w3-white",
-          { onsubmit: (e) => e.preventDefault() },
-          m(
-            "div.w3-section",
-            m("label", m("b", "Presentation Title")),
-            m("input.w3-input.w3-border-bottom", {
-              type: "text",
-              value: state.title,
-              oncreate: ({ dom }) => dom.focus(),
-              oninput: (e) => (state.title = e.target.value),
-            })
-          ),
-          m(
-            "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
-            { onclick: () => save({ mdl, state, key }) },
-            state.add ? "add" : "edit"
-          )
+          "div.w3-section",
+          m("label", m("b", "Presentation Title")),
+          m("input.w3-input.w3-border-bottom", {
+            type: "text",
+            value: state.title,
+            oncreate: ({ dom }) => dom.focus(),
+            oninput: (e) => (state.title = e.target.value),
+          })
         ),
-    }
-  }
-
-  const NewSlideForm = () => {
-    const state = {
-      title: "",
-    }
-
-    return {
-      view: ({ attrs: { mdl } }) =>
         m(
-          "form.w3-container.w3-card.w3-white",
-          { onsubmit: (e) => e.preventDefault() },
-          m(
-            "div.w3-section",
-            m("label", m("b", "Slide Title")),
-            m("input.w3-input.w3-border-bottom", {
-              type: "text",
-              value: state.title,
-              oncreate: ({ dom }) => dom.focus(),
-              oninput: (e) => (state.title = e.target.value),
-            })
-          ),
-          m(
-            "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
-            { onclick: () => addSlide(mdl, state) },
-            "add"
-          )
-        ),
-    }
+          "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
+          { onclick: () => save({ mdl, state, key }) },
+          state.add ? "add" : "edit"
+        )
+      ),
+  }
+}
+
+const NewSlideForm = () => {
+  const state = {
+    title: "",
   }
 
-  export { NewPresentationForm, NewSlideForm }
+  return {
+    view: ({ attrs: { mdl } }) =>
+      m(
+        "form.w3-container.w3-card.w3-white",
+        { onsubmit: (e) => e.preventDefault() },
+        m(
+          "div.w3-section",
+          m("label", m("b", "Slide Title")),
+          m("input.w3-input.w3-border-bottom", {
+            type: "text",
+            value: state.title,
+            oncreate: ({ dom }) => dom.focus(),
+            oninput: (e) => (state.title = e.target.value),
+          })
+        ),
+        m(
+          "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
+          { onclick: () => addSlide(mdl, state) },
+          "add"
+        )
+      ),
+  }
+}
+
+export { NewPresentationForm, NewSlideForm }
 
