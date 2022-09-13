3ac7452f064339b8aa16cc882bd96f41643428ed
bblake <bblake@goempyrean.com>
2022-09-13 10:04:39 -0500

    b

--- src/components/forms.js
+++ src/components/forms.js
@@ -1,115 +1,127 @@
 import m from "mithril"
-import { pluck } from 'ramda'
+import { compose, pluck } from 'ramda'
 import { SLIDE, PRESENTATION, loadAllPresentationsTask, loadSlidesByProjectId } from "../model"
 import { currentPresentationId } from '../helpers'
+import Task from "data.task/lib/task"
 
-const save = ({ mdl, state, key }) => key ? editPresentation(mdl, state, key) : addPresentation(mdl, state)
-
-const addPresentation = (mdl, state) => {
-  const presentation = PRESENTATION(state.title)
-
-  const onError = log("error")
-  const onSuccess = () => {
-    state.title = ""
-    mdl.state.showModal = false
-    loadAllPresentationsTask(mdl)
-  }
-
+const validateStateTask = (state) => {
   const titles = pluck('title', mdl.presentations)
-  if (titles.includes(presentation.title)) {
-    return alert('Title is not uniqe')
+  // state.title ? titles.includes(state.title) && alert('Title is not uniqe') : alert('Title is required')
+  if (state.title) {
+    if (titles.includes(state.title)) {
+      return Task.rejected(('Title is not uniqe'))
+    }
   } else {
-    mdl.http.postTask(mdl, "presentations", presentation).fork(onError, onSuccess)
+    return Task.reject(('Title is required'))
   }
+  return Task.of(state)
 }
 
-const editPresentation = (mdl, state, key) => {
-
-  const onError = log("error")
-  const onSuccess = () => {
-    state.title = ""
-    mdl.state.showModal = false
-    loadAllPresentationsTask(mdl)
-  }
-
-  const titles = pluck('title', mdl.presentations)
-  title ? titles.includes(state.title) && alert('Title is not uniqe') : alert('Title is required')
-
-
-  mdl.http.postTask(mdl, `presentations/${key}`, { title }).fork(onError, onSuccess)
+const addPresentationTask = ({ mdl, state }) => {
+  const presentation = PRESENTATION(state.title)
+  return mdl.http.postTask(mdl, "presentations", presentation)
 }
 
+const editPresentationTask = ({ mdl, state: { title }, key }) => {
+  mdl.http.postTask(mdl, `presentations/${key}`, { title })
 
-const addSlide = (mdl, state) => {
-  const slide = SLIDE(state.title, currentPresentationId())
 
-  const onError = log("error")
-  const onSuccess = () => {
-    state.title = ""
-    mdl.state.showModal = false
-    loadSlidesByProjectId(mdl, currentPresentationId())
+  const save = ({ mdl, state, key }) => {
+    const onError = error => {
+      state.error = error;
+      alert(state.error)
+    }
+
+    const onSuccess = () => {
+      state.title = ""
+      mdl.state.showModal = false
+      loadAllPresentationsTask(mdl)
+    }
+
+    validateStateTask(state).chain(state =>
+      state.add ? addPresentationTask({ mdl, state }) : editPresentationTask({ mdl, state, key })
+    ).fork(onError, onSuccess)
   }
 
-  mdl.http.postTask(mdl, "slides", slide).fork(onError, onSuccess)
-}
 
-const NewPresentationForm = ({ attrs: { title } }) => {
-  const state = {
-    title: title ? title : "",
+
+
+  const addSlide = (mdl, state) => {
+    const slide = SLIDE(state.title, currentPresentationId())
+
+    const onError = log("error")
+    const onSuccess = () => {
+      state.title = ""
+      mdl.state.showModal = false
+      loadSlidesByProjectId(mdl, currentPresentationId())
+    }
+
+    mdl.http.postTask(mdl, "slides", slide).fork(onError, onSuccess)
   }
 
-  return {
-    view: ({ attrs: { mdl, key } }) =>
-      m(
-        "form.w3-container.w3-card.w3-white",
-        { onsubmit: (e) => e.preventDefault() },
+  const NewPresentationForm = ({ attrs: { title } }) => {
+    const state = {
+      title: "",
+      add: true
+    }
+
+    if (title) {
+      state.title = title
+      state.add = false
+    }
+
+    return {
+      view: ({ attrs: { mdl, key } }) =>
         m(
-          "div.w3-section",
-          m("label", m("b", "Presentation Title")),
-          m("input.w3-input.w3-border-bottom", {
-            type: "text",
-            value: state.title,
-            oncreate: ({ dom }) => dom.focus(),
-            oninput: (e) => (state.title = e.target.value),
-          })
+          "form.w3-container.w3-card.w3-white",
+          { onsubmit: (e) => e.preventDefault() },
+          m(
+            "div.w3-section",
+            m("label", m("b", "Presentation Title")),
+            m("input.w3-input.w3-border-bottom", {
+              type: "text",
+              value: state.title,
+              oncreate: ({ dom }) => dom.focus(),
+              oninput: (e) => (state.title = e.target.value),
+            })
+          ),
+          m(
+            "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
+            { onclick: () => save({ mdl, state, key }) },
+            state.add ? "add" : "edit"
+          )
         ),
-        m(
-          "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
-          { onclick: () => save({ mdl, state, key }) },
-          "add"
-        )
-      ),
-  }
-}
-
-const NewSlideForm = () => {
-  const state = {
-    title: "",
+    }
   }
 
-  return {
-    view: ({ attrs: { mdl } }) =>
-      m(
-        "form.w3-container.w3-card.w3-white",
-        { onsubmit: (e) => e.preventDefault() },
+  const NewSlideForm = () => {
+    const state = {
+      title: "",
+    }
+
+    return {
+      view: ({ attrs: { mdl } }) =>
         m(
-          "div.w3-section",
-          m("label", m("b", "Slide Title")),
-          m("input.w3-input.w3-border-bottom", {
-            type: "text",
-            value: state.title,
-            oncreate: ({ dom }) => dom.focus(),
-            oninput: (e) => (state.title = e.target.value),
-          })
+          "form.w3-container.w3-card.w3-white",
+          { onsubmit: (e) => e.preventDefault() },
+          m(
+            "div.w3-section",
+            m("label", m("b", "Slide Title")),
+            m("input.w3-input.w3-border-bottom", {
+              type: "text",
+              value: state.title,
+              oncreate: ({ dom }) => dom.focus(),
+              oninput: (e) => (state.title = e.target.value),
+            })
+          ),
+          m(
+            "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
+            { onclick: () => addSlide(mdl, state) },
+            "add"
+          )
         ),
-        m(
-          "button.w3-button.w3-block.w3-orange.w3-section.w3-padding",
-          { onclick: () => addSlide(mdl, state) },
-          "add"
-        )
-      ),
+    }
   }
-}
 
-export { NewPresentationForm, NewSlideForm }
+  export { NewPresentationForm, NewSlideForm }
 
